816c716c36f21d04b2d664d19f163489edade99f
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-05-06 23:27:39 -0400

    update

--- src/components/PopularSeries.jsx
+++ src/components/PopularSeries.jsx
@@ -1,12 +1,35 @@
+import { useEffect } from "react";
 import LoadingAnimation from "./LoadingAnimation.jsx";
 
 export default function PopularSeries() {
+  useEffect(() => {
+    const url = "https://myanimelist.p.rapidapi.com/anime/top/all";
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "5d8ef6b029mshdf231aa011b282ep1f99a7jsn1bdc6f4d638a",
+        "X-RapidAPI-Host": "myanimelist.p.rapidapi.com",
+      },
+    };
+
+    const getPopularAnime = async () => {
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getPopularAnime();
+  }, []);
+
   return (
     <section className="popular-series">
       <div className="popular">
         <h2>Most Popular Anime</h2>
         <div className="carousel" id="popular-anime">
-          <LoadingAnimation />
+          {/* {<LoadingAnimation /> } */}
         </div>
       </div>
       <div className="popular">
