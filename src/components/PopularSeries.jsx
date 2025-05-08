8d5d10ea2bf517db8aebd90163ee678ec77ac44f
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-05-08 10:41:24 -0400

    fetching attempt

--- src/components/PopularSeries.jsx
+++ src/components/PopularSeries.jsx
@@ -1,36 +1,27 @@
-// import { useEffect, useState } from "react";
 import LoadingAnimation from "./LoadingAnimation.jsx";
 
 export default function PopularSeries() {
-  // const url = "https://myanimelist.p.rapidapi.com/anime/top/all";
-  // const options = {
-  //   method: "GET",
-  //   headers: {
-  //     "X-RapidAPI-Key": "5d8ef6b029mshdf231aa011b282ep1f99a7jsn1bdc6f4d638a",
-  //     "X-RapidAPI-Host": "myanimelist.p.rapidapi.com",
-  //   },
-  // };
+  // use tanstack query to fetch from API, tutorial video is below
+  // https://youtu.be/e74rB-14-m8?si=LfITnv-nhdUT4716
+  const url = "https://myanimelist.p.rapidapi.com/anime/top/all";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "5d8ef6b029mshdf231aa011b282ep1f99a7jsn1bdc6f4d638a",
+      "X-RapidAPI-Host": "myanimelist.p.rapidapi.com",
+    },
+  };
 
-  fetch("https://fakestoreapi.com/products/1")
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-  // const [data, setData] = useState([]);
-  // const [error, setError] = useState(null);
-
-  // useEffect(() => {
-  //   fetch("https://api.example.com/data")
-  //     .then((response) => response.json())
-  //     .then((data) => setData(data))
-  //     .catch((error) => setError(error));
-  // }, []);
+  const response = fetch(url, options);
+  const result = response.json();
+  console.log(result);
 
   return (
     <section className="popular-series">
       <div className="popular">
         <h2>Most Popular Anime</h2>
         <div className="carousel" id="popular-anime">
-          <LoadingAnimation />
+          {}
         </div>
       </div>
       <div className="popular">
