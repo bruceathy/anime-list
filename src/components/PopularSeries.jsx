6b876f1c50160cc1c78afdd978ec33deee7899ad
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-05-11 23:37:19 -0400

    err update

--- src/components/PopularSeries.jsx
+++ src/components/PopularSeries.jsx
@@ -1,18 +1,18 @@
 export default function PopularSeries() {
   // use tanstack query to fetch from API, tutorial video is below
   // https://youtu.be/e74rB-14-m8?si=LfITnv-nhdUT4716
-  const url = "https://myanimelist.p.rapidapi.com/anime/top/all";
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "5d8ef6b029mshdf231aa011b282ep1f99a7jsn1bdc6f4d638a",
-      "X-RapidAPI-Host": "myanimelist.p.rapidapi.com",
-    },
-  };
+  // const url = "https://myanimelist.p.rapidapi.com/anime/top/all";
+  // const options = {
+  //   method: "GET",
+  //   headers: {
+  //     "X-RapidAPI-Key": "5d8ef6b029mshdf231aa011b282ep1f99a7jsn1bdc6f4d638a",
+  //     "X-RapidAPI-Host": "myanimelist.p.rapidapi.com",
+  //   },
+  // };
 
-  const response = fetch(url, options);
-  const result = response.json();
-  console.log(result);
+  // const response = fetch(url, options);
+  // const result = response.json();
+  // console.log(result);
 
   return (
     <section className="popular-series">
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,15 +2,11 @@ import { Link } from "react-router-dom";
 import MainNav from "../components/MainNav";
 import Footer from "../components/Footer";
 
-import jjkleft from "../assets/jjk-left.jpeg";
-import jjkright from "../assets/jjk-right.jpeg";
-
 export default function ErrorPage() {
   return (
     <>
       <MainNav />
       <div className="err-container">
-        <img className="sidebar left" src={jjkleft} />
         <main className="error">
           <h1>Error 404</h1>
           <p>Page not found</p>
@@ -18,7 +14,6 @@ export default function ErrorPage() {
             Return Home
           </Link>
         </main>
-        <img className="sidebar right" src={jjkright} />
       </div>
       <Footer />
     </>
