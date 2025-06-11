20c23f4c188068ce3af7538175c49682e5c0ecbb
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-06-11 10:20:19 -0400

    reset

--- src/components/AniRevSec.jsx
+++ src/components/AniRevSec.jsx
@@ -1,27 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import LoadingAnimation from "../components/LoadingAnimation";
 import RevCard from "./RevCard";
-import { useParams } from "react-router-dom";
+
+const url =
+  "https://myanimelist.p.rapidapi.com/v2/anime/reviews?p=1&spoilers=false&preliminary=true&include_tags=recommended&exclude_tags=creative%2Cnot_recommended";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5d8ef6b029mshdf231aa011b282ep1f99a7jsn1bdc6f4d638a",
+    "X-RapidAPI-Host": "myanimelist.p.rapidapi.com",
+  },
+};
+
+const fetchAnimeRev = async () => {
+  const response = await fetch(url, options);
+  if (!response.ok) throw new Error("Network response wasn't ok");
+  return response.json();
+};
 
 export default function AniRevSec() {
-  const { page } = useParams(); // Get the page number from the URL parameter
-  const pageNumber = parseInt(page) || 1; // Parse the page number as an integer, default to 1
-
-  const url = `https://myanimelist.p.rapidapi.com/v2/anime/reviews?p=${pageNumber}&spoilers=false&preliminary=true&include_tags=recommended&exclude_tags=creative%2Cnot_recommended`;
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "5d8ef6b029mshdf231aa011b282ep1f99a7jsn1bdc6f4d638a",
-      "X-RapidAPI-Host": "myanimelist.p.rapidapi.com",
-    },
-  };
-
-  const fetchAnimeRev = async () => {
-    const response = await fetch(url, options);
-    if (!response.ok) throw new Error("Network response wasn't ok");
-    return response.json();
-  };
-
   const { isLoading, data, error } = useQuery({
     queryKey: ["animeRev"],
     queryFn: fetchAnimeRev,
