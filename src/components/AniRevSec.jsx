0aa9c67999c75c53b63c87f140577995a0acd065
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-06-28 23:58:58 -0400

    up

--- src/components/AniRevSec.jsx
+++ src/components/AniRevSec.jsx
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import LoadingAnimation from "../components/LoadingAnimation";
 import RevCard from "./RevCard";
 
