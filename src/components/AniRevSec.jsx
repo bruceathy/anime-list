b449de992549adc8d9f41da45cb8dfe4190bd8b7
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-07-27 23:48:55 -0400

    up

--- src/components/AniRevSec.jsx
+++ src/components/AniRevSec.jsx
@@ -9,9 +9,9 @@ const fetchAnimeRev = async (page = 1) => {
   const options = {
     method: "GET",
     headers: {
-      // remove api kan and add it to a .env file
+      remove api kan and add it to a .env file
       "X-RapidAPI-Key": "5d8ef6b029mshdf231aa011b282ep1f99a7jsn1bdc6f4d638a",
-      "X-RapidAPI-Host": "myanimelist.p.rapidapi.com",
+      "X-RapidAPI-Host": "myanimelist.p.rapidapi.com"
     },
   };
 
