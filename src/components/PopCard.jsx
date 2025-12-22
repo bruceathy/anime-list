5b457aae6780f89013062ccb460f49e6648b45f8
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-12-22 10:26:32 -0500

    setup

--- src/components/PopCard.jsx
+++ src/components/PopCard.jsx
@@ -1,15 +1,16 @@
-import { Link } from "react-router-dom";
 import AddBtn from "./AddBtn";
 // A POPUP WITH MORE INFO ABOUT THE SERIES SHOULD APPEAR WHEN CLICKING ON THE SERIES TITLE
 
-export default function PopCard({ title, picture_url, myanimelist_url }) {
+export default function PopCard({ title, picture_url }) {
+  function showMore() {
+    alert(`This is ${title}`);
+  }
+
   return (
     <div className="card" style={{ backgroundImage: `url(${picture_url})` }}>
       <div className="info">
         <h3>
-          <Link to={myanimelist_url} target="_blank">
-            {title}
-          </Link>
+          <a onClick={showMore}>{title}</a>
         </h3>
         <AddBtn />
       </div>
