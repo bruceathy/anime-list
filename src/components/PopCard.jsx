c0674102bf1e0e122456452f755b2bfd0b848f27
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-12-22 10:44:33 -0500

    popup

--- src/components/PopCard.jsx
+++ src/components/PopCard.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import AddBtn from "./AddBtn";
 // A POPUP WITH MORE INFO ABOUT THE SERIES SHOULD APPEAR WHEN CLICKING ON THE SERIES TITLE
 
 export default function PopCard({ title, picture_url }) {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
   function showMore() {
-    alert(`This is ${title}`);
+    // alert(`This is ${title}`);
+    setIsPopupOpen(true);
+  }
+
+  function closePopup() {
+    setIsPopupOpen(false);
   }
 
   return (
@@ -12,6 +20,14 @@ export default function PopCard({ title, picture_url }) {
         <h3>
           <a onClick={showMore}>{title}</a>
         </h3>
+        {isPopupOpen && (
+          <div className="popup">
+            <div className="popup-content">
+              <p>{title}</p>
+              <button onClick={closePopup}>X</button>
+            </div>
+          </div>
+        )}
         <AddBtn />
       </div>
     </div>
