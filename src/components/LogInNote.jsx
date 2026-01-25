34ca334422a85556288e0c90958187cf655d2610
bruceathy <67832593+bruceathy@users.noreply.github.com>
2026-01-24 22:26:09 -0500

    UP

--- src/components/LogInNote.jsx
+++ src/components/LogInNote.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function LogInNote() {
   return (
     <div className="login-note">
-      <h2>Log in to save your favorite anime and manga</h2>
+      <h2>Log in to save your favorite anime and manga!</h2>
       <Link to="/login" className="login-note-link">
         Login / Sign Up
       </Link>
