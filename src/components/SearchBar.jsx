dcb67fcf834fe78790c38faec872e16ae85debc9
bruceathy <67832593+bruceathy@users.noreply.github.com>
2026-04-01 10:45:17 -0400

    search start

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,27 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchBar() {
   const [search, setSearch] = useState("");
+  const [type, setType] = useState("anime");
+  const navigate = useNavigate();
 
-  function searchlist() {
-    alert(`search: ${search}`);
+  function handleSearch() {
+    if (search.trim()) return;
+    navigate(`/search?q=${encodeURIComponent(search.trim())}
+     +&type=${type}`);
     setSearch("");
   }
 
+  // function handleKeyDown(e) {
+  //   if (e.key === "Enter") {
+  //     handleSearch();
+  //   }
+  // }
+
   return (
     <div className="searchbar-container">
-      <button className="search-btn" id="search-btn" onClick={searchlist}>
+      <button className="search-btn" id="search-btn" onClick={handleSearch}>
         <i className="fas fa-search"></i>
       </button>
 
