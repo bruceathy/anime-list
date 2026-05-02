0b4e7f7f1e2f1ce8be60d1460d005635a708d442
bruceathy <67832593+bruceathy@users.noreply.github.com>
2026-05-01 23:58:50 -0400

    up

--- src/components/loading-elements/LoadingPopCard.jsx
+++ src/components/loading-elements/LoadingPopCard.jsx
@@ -1,3 +1,9 @@
 export default function LoadingPopCard() {
-  return <div className="pop-series-bg"></div>;
+  return <div className="pop-series-bg">
+    <div className="pop-series-container"></div>
+      <div className="pop-series-info">
+        <h3>Loading...</h3>
+      </div>
+    </div>
+  </div>;
 }
