666837e2aeea147299542a019de7bdfb56cc4c2e
bruceathy <67832593+bruceathy@users.noreply.github.com>
2026-06-30 10:46:40 -0400

    search results

--- src/components/AnimeResults.jsx
+++ src/components/AnimeResults.jsx
@@ -1,8 +1,42 @@
+import LoadingHomeCard from "./loading-elements/LoadingHomeCard";
 export default function AnimeResults() {
   return (
     <div className="results-container">
       <h3>Anime search results for: </h3>
-      <div className=""></div>
+      <div className="">
+        <ul style={{ display: "flex" }}>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/MangaResults.jsx
+++ src/components/MangaResults.jsx
@@ -1,12 +1,40 @@
+import LoadingHomeCard from "./loading-elements/LoadingHomeCard";
 export default function MangaResults() {
   return (
     <div className="results-container">
       <h3>Manga search results for: </h3>
       <div className="">
-        <ul>
-          <li>test item</li>
-          <li>test item</li>
-          <li>test item</li>
+        <ul style={{ display: "flex" }}>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
+          <li>
+            <LoadingHomeCard />
+          </li>
         </ul>
       </div>
     </div>
